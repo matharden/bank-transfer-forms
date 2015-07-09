15e1a14ebe7e527c5d1adc6a5af6b7ea718f78a6
Mat Harden <matharden@gmail.com>
2015-07-09 23:05:15 +0100

    Provide basic login form
    Form requires any string for username and password and redirects to
    the chosen bank form.

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,16 @@
-var form = document.querySelector('form');
 
-form.setAttribute('action', '../summary.html');
+var getParameterByName = function(name){
+  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+    results = regex.exec(location.search);
+  return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+};
+
+
+// Hijack form action to summary view when JavaScript is available
+
+var form = document.querySelector('#transfer');
+
+if (form) {
+  form.setAttribute('action', '../summary.html');
+}
